4d6f7e0c05e01c4a57a8f41134d4b170637e4566
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-18 10:57:09 +0100

    Add collider functionality for xr left  hand

---
+++ Assets/Scripts/AnimateHands.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHands : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty _pinchAction;
+
+    [SerializeField] private InputActionReference _gripAction;
+
+    [SerializeField] private Animator _animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerVal = _pinchAction.action.ReadValue<float>();
+        _animator.SetFloat("Trigger", triggerVal);
+
+        float gripVal = _gripAction.action.ReadValue<float>();
+        _animator.SetFloat("Grip", gripVal);
+    }
+}
