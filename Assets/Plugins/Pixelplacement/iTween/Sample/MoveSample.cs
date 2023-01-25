e341495cae9b7b900b442c170225ecedf49a2b05
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-25 14:22:11 +0100

    Add linear movement to the big boat

--- Assets/Plugins/Pixelplacement/iTween/Sample/MoveSample.cs
+++ Assets/Plugins/Pixelplacement/iTween/Sample/MoveSample.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class MoveSample : MonoBehaviour
 {
+    private Vector3 _targetPosition;
+    [SerializeField] private GameObject _targetPoint;
+    [SerializeField] private float _speed = 1;
+
+    private void Awake()
+    {
+        _targetPosition = _targetPoint.transform.position;
+    }
+
     void Start()
     {
-        print("itwen works");
-        iTween.MoveBy(gameObject, iTween.Hash("x", 2, "easeType", "easeInOutExpo", "loopType", "pingPong", "delay", .1));
+        iTween.MoveTo(gameObject, iTween.Hash("position", _targetPosition, "speed", _speed, "easetype", "linear"));
     }
 }
 
